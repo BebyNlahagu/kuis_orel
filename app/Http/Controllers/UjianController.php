b140f43c4ae7cf105d58e5c2cc40193ce1b8c441
BebyNlahagu <bebynlahagu20@gmail.com>
2025-04-23 23:46:41 +0700

    akkaka

--- app/Http/Controllers/UjianController.php
+++ app/Http/Controllers/UjianController.php
@@ -22,27 +22,3 @@ class UjianController extends Controller
         return view('user.kuis.index', compact('kuis','jawaban','hasil'));
     }
 }
-
-<?php
-
-namespace App\Http\Controllers;
-
-use App\Models\Jawaban;
-use App\Models\Kuis;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
-class UjianController extends Controller
-{
-    public function index()
-    {
-        $user = Auth::user();
-        $kuis = Kuis::all();
-        $jawaban = Jawaban::where('user_id',$user->id)->get();
-
-        $total = $jawaban->sum('skor');
-
-        $hasil = $total;
-        return view('user.kuis.index', compact('kuis','jawaban','hasil'));
-    }
-}
